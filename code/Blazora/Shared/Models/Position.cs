cbed79d9562872c50c0669734ff785162c3b8489
marckruzik <marc.kruzik@gmail.com>
2021-05-30 19:46:57 +0200

    wip

---
+++ code/Blazora/Shared/Models/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared.Models
+{
+    public class Position
+    {
+        public int x;
+        public int y;
+        public int width;
+        public int height;
+
+        public Position (int x, int y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
---
+++ code/Blazora/wwwroot/js/function.js
@@ -0,0 +1,4 @@
+function hover_color_set(id, color)
+{
+    $(id).css("background-color", color);
+}
