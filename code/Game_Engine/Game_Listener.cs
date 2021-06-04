1700768cbdf27d438e7010e41d162732eca96f43
marckruzik <marc.kruzik@gmail.com>
2021-06-04 11:25:42 +0200

    Manager Listener

---
+++ code/Game_Engine/Game_Listener.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NS_Game_Engine
+{
+
+
+    public class Game_Listener<T>
+    {
+        List<KeyValuePair<Func<T>, T>> list_kvp_func_val = new List<KeyValuePair<Func<T>, T>> ();
+
+        public void add (Func<T> func)
+        {
+            if (func != null)
+            {
+                list_kvp_func_val.Add (new KeyValuePair<Func<T>, T> (func, default(T)));
+            }
+        }
+
+
+        public bool need_update ()
+        {
+            for (int i = 0; i < this.list_kvp_func_val.Count; i++)
+            {
+                KeyValuePair<Func<T>, T> kvp_func_val = list_kvp_func_val[i];
+
+                Func<T> func = kvp_func_val.Key;
+                T func_val = func ();
+                T val = kvp_func_val.Value;
+                if (func_val.Equals (val) == false)
+                {
+                    list_kvp_func_val[i] = new KeyValuePair<Func<T>, T> (func, func_val);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ code/Game_Engine/Manager_Listener.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace NS_Game_Engine
+{
+    public class Manager_Listener
+    {
+        /*
+        Dictionary<Type, IGame_Listener> dico_typ_game_listener = new Dictionary<Type, IGame_Listener> ();
+
+        public void add<T> (Func<T> func)
+        {
+            if (dico_typ_game_listener.ContainsKey (typeof(T)) == false)
+            {
+                dico_typ_game_listener.Add (typeof (T), new Game_Listener<T> ());
+            }
+            var x = dico_typ_game_listener[typeof (T)];
+        }
+        */
+
+        Game_Listener<int> listener_int = new Game_Listener<int> ();
+        Game_Listener<string> listener_string = new Game_Listener<string> ();
+        Game_Listener<bool> listener_bool = new Game_Listener<bool> ();
+
+        public void add<T> (Func<T> func)
+        {
+            Game_Listener<T> game_listener = get_listener<T> ();
+            game_listener.add (func);
+        }
+
+        private Game_Listener<T> get_listener<T> ()
+        {
+            if (typeof (T) == typeof (int))
+            {
+                return this.listener_int as Game_Listener<T>;
+            }
+            else if (typeof (T) == typeof (string))
+            {
+                return this.listener_string as Game_Listener<T>;
+            }
+            else if (typeof (T) == typeof (bool))
+            {
+                return this.listener_bool as Game_Listener<T>;
+            }
+            throw new Exception ("unknown listener type : " + typeof(T).ToString ());
+        }
+
+        public bool need_update ()
+        {
+            bool need_update = false;
+            need_update |= this.listener_int.need_update ();
+            need_update |= this.listener_string.need_update ();
+            need_update |= this.listener_bool.need_update ();
+            return need_update;
+        }
+
+    }
+}
